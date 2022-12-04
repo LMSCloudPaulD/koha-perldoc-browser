42ef4eaecd808ac7f8d77e369ad167247630cd74
LMSCloudPaulD <paul.derscheid@lmscloud.de>
2022-12-04 19:11:11 +0100

    Pull updates | Sun  4 Dec 2022 19:11:08 CET

--- build.sh
+++ build.sh
@@ -2,39 +2,56 @@
 
 echo "Starting up.."
 
-SCRIPT_NAME=$(readlink -f "$0")
-SCRIPT_PATH=$(dirname "$SCRIPT_NAME")
+# Get the path of the script and change into that directory
+SCRIPT_PATH=$(cd "$(dirname "$0")" && pwd)
+cd "$SCRIPT_PATH" || exit 1
+
+# Set the repository URL and date
 REPOSITORY='https://github.com/Koha-Community/Koha.git'
 DATE=$(date)
-# LINK='<link rel="stylesheet" href="\.\.\/main.css">'
-# SCRIPT='<script src="\.\.\/kohaPerldoc\.js"><\/script>'
 
 echo "Globals set.."
 
-echo "$SCRIPT_PATH"
-
+# Change into the Koha directory
 echo "Changing into Koha/ .."
-cd "$SCRIPT_PATH/Koha" || exit 1
+cd Koha || exit 1
 
 echo "Pulling Koha repository"
+
+# Reset the repository to the latest version on origin/master
+# and then pull the latest changes
 git reset --hard origin/master || exit 1
 git pull "$REPOSITORY" || exit 1
 
-echo "Chaning back into root directory.."
+echo "Changing back into root directory.."
 cd "$SCRIPT_PATH" || exit 1
 
+# Generate the documentation
 echo "Generating documentation"
 perl -MPod::Simple::HTMLBatch -e Pod::Simple::HTMLBatch::go Koha docs || exit 1
 
+# Copy the main.css and kohaPerldoc.js files to the docs directory
 cp main.css docs/ || exit 1
 cp kohaPerldoc.js docs/ || exit 1
 
-# use regular sed on Linux; macOS uses BSD sed by default and acts strange
-echo "Inserting CSS & JS"
-INDEX_HTML=$(gsed "s/^<\/head>/<link rel=\"stylesheet\" href=\"main.css\"><script defer src=\"kohaPerldoc\.js\"><\/script><\/head>/g" "$SCRIPT_PATH/docs/index.html")
+# Check if the host OS is M1 macOS
+if [ "$(uname -p)" = "arm64" ]; then
+    # Check if gsed is installed
+    if ! command -v gsed > /dev/null; then
+        # Show the user how to install gsed
+        echo "gsed is not installed. Please run 'brew install gnu-sed' to install it and then try again."
+        exit 1
+    fi
+    # Use gsed to insert the CSS and JS into the index.html file
+    echo "Inserting CSS & JS"
+    gsed -i "s/^<\/head>/<link rel=\"stylesheet\" href=\"main.css\"><script defer src=\"kohaPerldoc\.js\"><\/script><\/head>/g" "$SCRIPT_PATH/docs/index.html"
+else
+    # Use sed to insert the CSS and JS into the index.html file
+    echo "Inserting CSS & JS"
+    sed -i "s/^<\/head>/<link rel=\"stylesheet\" href=\"main.css\"><script defer src=\"kohaPerldoc\.js\"><\/script><\/head>/g" "$SCRIPT_PATH/docs/index.html"
+fi
 
-echo "$INDEX_HTML" >"$SCRIPT_PATH/docs/index.html"
-
+# Add, commit, and push the changes to the repository
 git add . || exit 1
 git commit -m "Pull updates | $DATE" || exit 1
 git push origin main || exit 1
